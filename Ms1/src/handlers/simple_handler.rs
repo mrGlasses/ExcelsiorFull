1e39b0d3e5dfbab2c2489fca32bbcf11cd2ca5dc
Nick <nicko.guimaraes@gmail.com>
2025-04-06 23:15:15 -0300

    MS1 and MS2 are communicating

--- Ms1/src/handlers/simple_handler.rs
+++ Ms1/src/handlers/simple_handler.rs
@@ -26,3 +26,26 @@ pub async fn get_pong(State(_state): State<AppState>) -> Json<Message> {
     msg.message_text = "PONG!".to_string();
     Json(msg)
 }
+
+pub async fn call_external_service() -> Json<Message> {
+    let url = "http://localhost:3001/pong";                      
+    let response = reqwest::get(url).await;                      
+
+    match response {                                             
+        Ok(resp) => {                                           
+            let json: Message = resp.json().await.unwrap_or(Message {
+                code: 400,
+                message_text: "Failed to parse external response".into(),
+            });
+            let new_message = Message{
+                code: json.code,
+                message_text: json.message_text + "!!"
+            };
+            Json(new_message)
+        },                                                                              
+        Err(_) => Json(Message {
+            code: 400,
+            message_text: "Failed to reach external service".into(),
+        })                                                                              
+    }                                                                                   
+}  
--- Ms1/src/routes/mod.rs
+++ Ms1/src/routes/mod.rs
@@ -7,5 +7,6 @@ pub fn create_routes(state: AppState) -> Router {
         .route("/users", get(user_handler::get_users))
         .route("/users", post(user_handler::create_user))
         .route("/ping", get(simple_handler::get_pong))
+        .route("/itsaRainyDay", get(simple_handler::call_external_service))
         .with_state(state)
 }
