61025afd013d55490b4586f8f4371cf0fce9c912
Nick <nicko.guimaraes@gmail.com>
2025-07-09 00:49:23 -0300

    1.1.3 - Default as a trait

--- Ms1/src/domain/general.rs
+++ Ms1/src/domain/general.rs
@@ -1,20 +1,12 @@
 use serde::{Deserialize, Serialize};
 use sqlx::FromRow;
 
-#[derive(Serialize, Deserialize, FromRow, Debug)]
+#[derive(Serialize, Deserialize, FromRow, Debug, Default)]
 pub struct Message {
     pub code: i32,
     pub message_text: String,
 }
 
-impl Default for Message {
-    fn default() -> Message {
-        Message {
-            code: Default::default(),
-            message_text: Default::default(),
-        }
-    }
-}
 
 #[derive(Deserialize)]
 pub struct Params {
