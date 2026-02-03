e83f42db7bd5a0eed141b585d33bb69f52512d6a
Nick <nicko.guimaraes@gmail.com>
2026-02-03 03:00:43 -0300

    2.1.0 - EX0001 - Logo in a txt file - still need the sad way test and maybe a consts file

--- src/utils/un_utils.rs
+++ src/utils/un_utils.rs
@@ -1,13 +1,32 @@
+use crate::domain::general::ReplaceParams;
+use tokio::fs;
+
 pub async fn start_message(location: String) {
-    println!("             ___");
-    println!(" ____       /_ _\\ ____   ____   _      ____   _____   _____   ____   ");
-    println!("|  __|  _    / / /   _| |  __| | |    / ___| |_   _| /  _  \\ |  _ \\ ");
-    println!("| |__  \\ \\  / / /  /    | |__  | |    | |__    | |   | | | | | |_| |");
-    println!("|  __|  \\ \\/ /  |  |    |  __| | |    \\__  \\   | |   | | | | |    / ");
-    println!("| |__   / /\\ \\  \\  \\__  | |__  | |__   __| |  _| |_  | |_| | | |\\ \\ ");
-    println!(
-        "|____| |_/  \\_|  \\____| |____| |____| |____/ |_____| \\_____/ |_| \\_\\   ver. {}", env!("CARGO_PKG_VERSION")
-    );
+    //read the logo.txt file, then print the logo
+    let mut param_vec = Vec::new();
+    let param = ReplaceParams {
+        old_str: "[VERSION]".to_string(),
+        new_str: env!("CARGO_PKG_VERSION").parse().unwrap(),
+    };
+    param_vec.push(param);
+
+    match fs::read_to_string("src/utils/logo.txt").await {
+        Ok(content) => {
+            let content = replace_bulk(content, &param_vec).await;
+            print!("{}", content);
+        }
+        Err(e) => {
+            eprintln!("Failed to read logo.txt: {}", e);
+        }
+    }
     println!();
     println!("Server listening on {}.", location)
+}
+
+async fn replace_bulk(text: String, params: &[ReplaceParams]) -> String {
+    let mut text_result = text;
+    for param in params {
+        text_result = text_result.replace(&param.old_str, &param.new_str);
+    }
+    text_result
 }
